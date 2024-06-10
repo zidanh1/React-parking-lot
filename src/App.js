6e980dd27c1aa5c1b5206ded52b1bc28d31603d8
Hashem Zidan <HashemZidan@MacBook-Pro.local>
2024-06-10 14:25:05 -0400

    tagline

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
       <header>
         <h1>Browser Parking Lot</h1>
+        <p>Send most of your browser tabs into retirment</p>
       </header>
     </div>
   );
