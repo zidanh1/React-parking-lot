e06478758667d5529058c821da401cf2679f5887
Hashem Zidan <HashemZidan@MacBook-Pro.local>
2024-06-23 19:29:35 -0400

    parking lot list

--- src/Components/ParkingLotList/ParkingLotList.jsx
+++ src/Components/ParkingLotList/ParkingLotList.jsx
@@ -1,5 +1,16 @@
-import React from "react";
+import React from 'react';
+import ParkingLotItem from './ParkingLotItem';
+import './ParkingLot.css';
 
-export default function ParkingLotList() {
-  return <div>ParkingLotList</div>;
+export default function ParkingLotList({ parkingLotItems }) {
+  
+  let <ParkingLotItemsJsxList = parkingLotItems.map(item => 
+  <ParkingLotItem key={item.id} {...item} />
+  );
+  
+    return (
+    <section className="parking-lot-list-container">
+        {ParkingLotItemsJsxList}
+        </section>
+  )
 }
